5a1decdcf6c35b313231a1b2a7bbab6058d367c7
juanemanuel153santos-spec <juanemanuel153santos@gmail.com>
2026-07-14 16:07:22 -0300

    configurar ALLOWED_HOSTS

--- motopecas/settings.py
+++ motopecas/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6-lvnq067^md-rhgi8!=bwn@hp41n9qoj!$8^5@d@rghtybvu@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
