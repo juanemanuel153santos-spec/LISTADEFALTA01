484ac471cf24714cc6db1e49150d760f423d3a69
juanemanuel153santos-spec <juanemanuel153santos@gmail.com>
2026-07-15 14:04:51 -0300

    configura PostgreSQL e criacao automatica de superusuario

--- build.sh
+++ build.sh
@@ -3,3 +3,14 @@
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+import os
+User = get_user_model()
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+if username and not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+"
