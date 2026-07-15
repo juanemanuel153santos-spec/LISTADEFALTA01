88ebbccbab4d0e261fdcbaf56da27a20fb926bcc
juanemanuel153santos-spec <juanemanuel153santos@gmail.com>
2026-07-15 14:02:15 -0300

    configura PostgreSQL para producao via DATABASE_URL

--- motopecas/settings.py
+++ motopecas/settings.py
@@ -27,6 +27,15 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600,
+    )
+}
+
 
 # Application definition
 
@@ -69,16 +78,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'motopecas.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
